dffedd46e3132a5edcbecfcb274d00cb09691d6d
Maximus Lewis <70774542+mslew@users.noreply.github.com>
2023-01-26 21:55:56 -0600

    testing the main branch

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -5,6 +5,7 @@ export default function test() {
         <div>
             <h1 className="text-yellow-400">This is a test :)</h1>
             <p className="font-thin">React is pretty cool!</p>
+            <p>TESTING MAIN</p>
         </div>
     )
 }
