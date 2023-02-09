1dbf3d00a42198dc5a1d40cb30afee245d97b54f
mslew <mlew3223@gmail.com>
2023-02-09 10:12:53 -0600

    small changes.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,34 +5,35 @@ import SignIn from "./icons/SignIn";
 import Register from "./icons/Register";
 import School from "./icons/School";
 import PenRuler from "./icons/PenRuler";
+import Q from "./icons/Q";
 
 export default function NavBar() {
     return(
-        <div className="fixed top-0 left-0 flex flex-row w-screen h-16 space-x-48 border-4 border-gray-200">
-            <div className="flex flex-col items-center">
-                <PenRuler />
-                <p>QuizMaster</p>
+        <div className="fixed top-0 left-0 flex flex-row w-screen h-16 space-x-48 overflow-auto border-4 border-gray-200">
+            <div className="flex flex-col items-center pl-2 space-y-2">
+                <Q />
+                <a className="text-black hover:text-red-400"><p>QuizMaster</p></a>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-col items-center space-y-2">
                 <House />
-                <p>Home</p>
+                <a><p>Home</p></a>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-col items-center space-y-2">
                 <Info />
-                <p>Information</p>
+                <a><p>Information</p></a>
             </div>
-            <div className="flex flex-row items-center">
-                <SignIn />
-                <p>Sign In</p>
-            </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-col items-center space-y-2">
                 <Register />
-                <p>Register</p>
+                <a><p>Register</p></a>
             </div>
-            <div className="flex flex-row items-center">
+            <div className="flex flex-col items-center space-y-2">
                 <School />
-                <p>Take a Quiz!</p>
+                <a><p>Take a Quiz!</p></a>
             </div>
+            <button className="flex flex-row items-center m-2 space-x-2">
+                <SignIn className="w-10 h-10"/>
+                <p>Sign In</p>
+            </button>
         </div>
     )
 }
