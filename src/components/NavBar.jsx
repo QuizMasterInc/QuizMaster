d309b2a64438cd609f6408c508a21dac5f57ce60
mslew <mlew3223@gmail.com>
2023-02-11 13:23:20 -0600

    changed positioning

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,35 +4,36 @@ import Info from "./icons/Info";
 import SignIn from "./icons/SignIn";
 import Register from "./icons/Register";
 import School from "./icons/School";
-import PenRuler from "./icons/PenRuler";
 import Q from "./icons/Q";
+import Bars from "./icons/Bars";
+import SignOut from "./icons/SignOut";
 
 export default function NavBar() {
     return(
-        <div className="fixed top-0 left-0 flex flex-row w-screen h-16 space-x-48 overflow-auto border-4 border-gray-200">
-            <div className="flex flex-col items-center pl-2 space-y-2">
+        <div className="fixed flex flex-col w-20 space-y-12 bg-gray-900 rounded-md left-2 top-2 bottom-2">
+            <div className="flex flex-col items-center p-2 mt-4 rounded-md">
                 <Q />
-                <a className="text-black hover:text-red-400"><p>QuizMaster</p></a>
             </div>
-            <div className="flex flex-col items-center space-y-2">
+            <button className="flex flex-col items-center m-1 bg-transparent hover:bg-gray-300">
+                <Bars />
+            </button>
+            <button className="flex flex-col items-center p-2 m-1 bg-transparent rounded-md hover:bg-gray-300">
                 <House />
-                <a><p>Home</p></a>
-            </div>
-            <div className="flex flex-col items-center space-y-2">
+            </button>
+            <button className="flex flex-col items-center p-2 m-1 bg-transparent rounded-md hover:bg-gray-300">
                 <Info />
-                <a><p>Information</p></a>
-            </div>
-            <div className="flex flex-col items-center space-y-2">
+            </button>
+            <button className="flex flex-col items-center p-2 m-1 bg-transparent rounded-md hover:bg-gray-300">
                 <Register />
-                <a><p>Register</p></a>
-            </div>
-            <div className="flex flex-col items-center space-y-2">
+            </button>
+            <button className="flex flex-col items-center p-2 m-1 bg-transparent rounded-md hover:bg-gray-300">
                 <School />
-                <a><p>Take a Quiz!</p></a>
-            </div>
-            <button className="flex flex-row items-center m-2 space-x-2">
-                <SignIn className="w-10 h-10"/>
-                <p>Sign In</p>
+            </button>
+            <button className="flex flex-col items-center p-2 m-1 space-x-2 bg-transparent rounded-md hover:bg-gray-300">
+                <SignIn />
+            </button>
+            <button className="flex flex-col items-center p-2 pb-4 m-1 space-x-2 bg-transparent rounded-md hover:bg-gray-300">
+                <SignOut />
             </button>
         </div>
     )
