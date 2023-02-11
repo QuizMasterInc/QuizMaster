2351c5ec72ea49444cc0d94021d577cbda01e338
mslew <mlew3223@gmail.com>
2023-02-11 15:18:32 -0600

    changed color of icons.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,22 +4,20 @@ import Info from "./icons/Info";
 import SignIn from "./icons/SignIn";
 import School from "./icons/School";
 import Q from "./icons/Q";
-import Bars from "./icons/Bars";
 import SignOut from "./icons/SignOut";
 import NavBarIcon from "./NavBarIcon";
 
 export default function NavBar() {
     return(
-        <div className="fixed flex flex-col w-20 space-y-12 bg-gray-900 rounded-md shadow-lg left-2 top-2 bottom-2">
+        <div className="fixed flex flex-col w-20 pb-4 space-y-12 bg-gray-900 rounded-md shadow-lg left-2 top-2 bottom-2">
             <div className="flex flex-col items-center p-2 mt-4 rounded-md">
                 <Q />
             </div>
-            <NavBarIcon icon={<Bars />} text={"Expand"}/>              
-            <NavBarIcon icon={<House />} text={"Home"} /> 
-            <NavBarIcon icon={<Info />} text={"Information"} /> 
-            <NavBarIcon icon={<School />} text={"Take a Quiz!"} /> 
-            <NavBarIcon icon={<SignIn />} text={"Sign In"} /> 
-            <NavBarIcon icon={<SignOut />} text={"Sign Out"} />
+            <NavBarIcon icon={<House className={"navbar-icon"} />} text={"Home"} /> 
+            <NavBarIcon icon={<Info className={"navbar-icon"} />} text={"Information"} /> 
+            <NavBarIcon icon={<School className={"navbar-icon"} />} text={"Take a Quiz!"} /> 
+            <NavBarIcon icon={<SignIn className={"navbar-icon"} />} text={"Sign In"} /> 
+            <NavBarIcon icon={<SignOut className={"navbar-icon"} />} text={"Sign Out"} />
         </div>
     )
 }
--- src/components/NavBarIcon.jsx
+++ src/components/NavBarIcon.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NavBarIcon = ({icon, text}) => (
-    <button className="navbar-icon group">
+    <button className="navbar-icon-button group">
         {icon}
         <span className="navbar-tooltip group-hover:scale-100">
             {text}
