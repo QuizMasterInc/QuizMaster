423e512dedccda5a61e5f6bd3888fe497b17efb4
mslew <mlew3223@gmail.com>
2023-02-15 00:25:14 -0600

    small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import NavBar from './components/navbar/NavBar'
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Test from './components/Test';
 import Test2 from './components/Test2';
 import NotFound from './components/404/NotFound'
