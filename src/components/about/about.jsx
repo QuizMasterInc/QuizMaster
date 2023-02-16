aad8d609a3ae9d5f2be9a17e7755d216826b9839
Anthony Mastores <mastores27@gmail.com>
2023-02-15 18:36:22 -0600

    small changes

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,7 +4,7 @@ import Logo from "../icons/logo.jpg";
 
 function About() {
   return (
-    <div className="bg-gray-800 shadow-lg hover:shadow-x text-gray-300 ">
+    <div className="bg-gray-800 shadow-lg hover:shadow-x text-gray-300 py-12">
       <div className="flex justify-center items-center">
         <img src={Logo} alt="logo" className="mx-auto w-76 h-64" />
       </div>
@@ -25,9 +25,9 @@ function About() {
         </div>
       </div>
       <div className="flex flex-col md:flex-row md:items-center mb-4" >
-        <div className="flex-1">
+        <div className="flex-1" >
           <div className="info-div">
-            <p>
+            <p className='py-4'>
               <b className = "text-gray-300 text-3xl" >Our Team</b> will be utilizing React.js, Firebase hosting, Tailwwind CSS styling, and Golang for back-end support. Firestore will be used to store and update quizzes and quiz takers results.
               <br></br>Our developers are open to feedback! Feel free to contact our team thorught the contact us page with any suggestions or recommendations to improve QuizMaster. 
             </p>
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -15,7 +15,7 @@ function App() {
   ]);
 
   return (
-    <div className="bg-gray-800 shadow-lg hover:shadow-x text-gray-300 ">
+    <div className="bg-gray-800 shadow-lg hover:shadow-x text-gray-300 pt-12">
       <div className='flex'>
       <div className="flex justify-center items-center w-1/2">
         <img src={Logo} alt="logo" className="mx-auto w-76 h-64" />
@@ -27,8 +27,8 @@ function App() {
       </div>
       <div className='flex'>
         <div className="flex justify-center items-center w-1/2">
-          <h1 className="text-3xl font-bold text-gray-300 mt-10 ">Subjects ranging from Mathematics, Science, English, and More!</h1>
-          <p className="text-2xl text-gray-300 mt-10 pl-2 ">The Perfect Quiz Taking Applicaiton for both Students and Teachers!</p>
+          <h1 className="text-3xl font-bold text-gray-300 mt-10 pl-8">Subjects ranging from Mathematics, Science, English, and More!</h1>
+          <p className="text-2xl text-gray-300 mt-10 pl-8 ">The Perfect Quiz Taking Applicaiton for both Students and Teachers!</p>
         </div>
         <div class="w-1/2 ">
           <div className="flex justify-center items-center ">
@@ -43,7 +43,7 @@ function App() {
       </div>
       <div class="w-1/2 ">
       <h1 className="text-5xl font-bold text-gray-300 mt-10 ">Ready to start the QuizMaster Experience? </h1>
-      <p className="text-2xl text-gray-300 mb-10 pt-8">Simply create an account or login if you already have an account. Navigate to the quizzes page and select a quiz. It's that easy!</p>
+      <p className="text-2xl text-gray-300 mb-10 pt-8 pr-8">Simply <a href = "/register"  className="underline">create an account</a> or <a href = "/login" className="underline">login</a> if you already have an account. Navigate to the <a href = "/quizzes"  className="underline">quizzes</a> page and select a quiz.</p>
       </div>
       </div>
     </div>
