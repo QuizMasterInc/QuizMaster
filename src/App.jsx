02a06946817dec96414ea8d696d2ca60dbdf523d
mslew <mlew3223@gmail.com>
2023-02-15 23:25:57 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
 import NavBar from './components/navbar/NavBar'
 import { Route, Routes } from "react-router-dom";
-import About from './components/about/About'
-import Contact from './components/contact/Contact';
-import Home from './components/home/Home';
-import NotFound from './components/404/NotFound'
+//import About from './components/about/About'
+//import Contact from './components/contact/Contact';
+//import Home from './components/home/Home';
+import NotFound from './components/404/NotFound';
 import SelectQuiz from './components/quizselect/SelectQuiz';
 import QuizTest from './components/quiz/QuizTest';
 
@@ -12,9 +12,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<NotFound />}/>
+        <Route path="/about" element={<NotFound />} />
+        <Route path="/contact" element={<NotFound />} />
         <Route path="/quizzes" element={<SelectQuiz />} />
         <Route path="/quizzes/:quiz" element={<QuizTest />}/>
         <Route path="*" element={<NotFound />} />
