c7a36addac46b8a540ddedfff9e0a9c4df18643f
mslew <mlew3223@gmail.com>
2023-02-15 23:53:01 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,18 @@ import { Route, Routes } from "react-router-dom";
 import NotFound from './components/404/NotFound';
 import SelectQuiz from './components/quizselect/SelectQuiz';
 import QuizTest from './components/quiz/QuizTest';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
+//import Home from './components/home/Home';
+//import About from './components/about/About';
+//import Contact from './components/contact/Contact';
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<QuizTest />}/>
+        <Route path="/about" element={<QuizTest />} />
+        <Route path="/contact" element={<QuizTest />} />
         <Route path="/quizzes" element={<SelectQuiz />} />
         <Route path="/quizzes/:quiz" element={<QuizTest />}/>
         <Route path="*" element={<NotFound />} />
