f73bbec661eafeaff25517ec46369ad9f18da2a1
mattespinos <matthewwespinos@lewisu.edu>
2023-02-15 23:49:17 -0600

    Updated pages

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Home from './components/home/Home';
 import NotFound from './components/404/NotFound'
 import SelectQuiz from './components/quizselect/SelectQuiz';
 import QuizTest from './components/quiz/QuizTest';
+import Login from './components/login/Login';
+import Register from './components/login/register';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="/quizzes" element={<SelectQuiz />} />
         <Route path="/quizzes/:quiz" element={<QuizTest />}/>
+        <Route path='/signin' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
       <NavBar />
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -20,7 +20,7 @@ function App() {
       <div className="flex items-center justify-center w-1/2">
         <img src={Logo} alt="logo" className="h-64 mx-auto w-76" />
       </div>
-      <div class="w-1/2 ">
+      <div className="w-1/2 ">
       <h1 className="mt-10 text-5xl font-bold text-gray-300 ">Welcome to QuizMaster</h1>
       <p className="pt-8 mb-10 text-2xl text-gray-300">The ultimate destination for all your quiz needs.</p>
       </div>
@@ -30,7 +30,7 @@ function App() {
           <h1 className="pl-8 mt-10 text-3xl font-bold text-gray-300">Subjects ranging from Mathematics, Science, English, and More!</h1>
           <p className="pl-8 mt-10 text-2xl text-gray-300 ">The Perfect Quiz Taking Applicaiton for both Students and Teachers!</p>
         </div>
-        <div class="w-1/2 ">
+        <div className="w-1/2 ">
           <div className="flex items-center justify-center ">
           <img src={Student} alt="student" className="h-64 mx-auto w-76" />
         </div>
@@ -41,7 +41,7 @@ function App() {
       <div className="flex items-center justify-center w-1/2">
         <img src={Quiz} alt="quiz" className="h-64 mx-auto w-76" />
       </div>
-      <div class="w-1/2 ">
+      <div className="w-1/2 ">
       <h1 className="mt-10 text-5xl font-bold text-gray-300 ">Ready to start the QuizMaster Experience? </h1>
       <p className="pt-8 pr-8 mb-10 text-2xl text-gray-300">Simply <a href = "/register"  className="underline">create an account</a> or <a href = "/login" className="underline">login</a> if you already have an account. Navigate to the <a href = "/quizzes"  className="underline">quizzes</a> page and select a quiz.</p>
       </div>
