82412b58def4a41041d1bdc4a90402ce393000dd
mslew <mlew3223@gmail.com>
2023-02-22 13:48:40 -0600

    question component only selects one now

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { useState } from "react";
 import QuestionChoice from "./QuestionChoice";
 
-const Question = ({questionText}) => (
+function Question ({questionText}){
+    const [activeIndex, setActiveIndex] = useState(null)
+    return (
     <div className="flex flex-col w-7/12 pb-4 mb-4 bg-gray-900 rounded-lg shadow-lg">
         <p className="flex pt-4 pb-4 pl-6 text-xl text-gray-300">{questionText}</p>
-        <QuestionChoice choiceText={"Choice test"}/>
-        <QuestionChoice choiceText={"Choice test"}/>
-        <QuestionChoice choiceText={"Choice test"}/>
-        <QuestionChoice choiceText={"Choice test"}/>
+        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 0} onSelect={() => setActiveIndex(0)}/>
+        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 1} onSelect={() => setActiveIndex(1)}/>
+        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 2} onSelect={() => setActiveIndex(2)}/>
+        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 3} onSelect={() => setActiveIndex(3)}/>
     </div>
-)
+    )
+}
 
 export default Question
--- src/components/quiz/QuestionChoice.jsx
+++ src/components/quiz/QuestionChoice.jsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { useState } from "react";
 
-function QuestionChoice (props){
-    const [isSelected, setSelected] = useState(false)
+function QuestionChoice ({choiceText, isSelected, onSelect}){
     return(
         <button className={`p-2 mb-3 ml-2 mr-2 text-gray-300 bg-gray-800 rounded-md shadow-md hover:bg-gray-600 ${isSelected ? "bg-gray-600" : "bg-gray-800"}`}
-        onClick={() => setSelected(!isSelected)}>{props.choiceText}</button>
+        onClick={onSelect}>{choiceText}</button>
     )
 }
 
