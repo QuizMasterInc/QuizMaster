dd8819f080f70688fb31308dbff95ee7e3b0407f
mslew <mlew3223@gmail.com>
2023-02-22 14:35:51 -0600

    formatted for future code

--- src/components/quiz/Question.jsx
+++ src/components/quiz/Question.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import { useState } from "react";
 import QuestionChoice from "./QuestionChoice";
 
-function Question ({questionText}){
+function Question ({questionText, choices, answer}){
     const [activeIndex, setActiveIndex] = useState(null)
     return (
+    <>
     <div className="flex flex-col w-7/12 pb-4 mb-4 bg-gray-900 rounded-lg shadow-lg">
         <p className="flex pt-4 pb-4 pl-6 text-xl text-gray-300">{questionText}</p>
-        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 0} onSelect={() => setActiveIndex(0)}/>
-        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 1} onSelect={() => setActiveIndex(1)}/>
-        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 2} onSelect={() => setActiveIndex(2)}/>
-        <QuestionChoice choiceText={"Choice test"} isSelected={activeIndex === 3} onSelect={() => setActiveIndex(3)}/>
+        {choices.map((choice, index) => (
+            <QuestionChoice key={index} choiceText={choice} isSelected={activeIndex === index} onSelect={() => setActiveIndex(index)}/>
+        ))} 
     </div>
+    <p>{answer}</p>
+    </>
     )
 }
 
--- src/components/quiz/QuizActivity.jsx
+++ src/components/quiz/QuizActivity.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import Question from "./Question";
-const QuizActivity = ({}) => (
-    <div className="flex flex-col justify-center">
-        <Question questionText={"Test"}/>
-        <Question questionText={"Test 2"}/>
+function QuizActivity({}){
+    const [questionText] = useState([
+        "Test",
+        "Test22"
+    ])
+
+    const [choices] = useState([
+        ["Test1", "Test2", "Test3", "Test4"],
+        ["Test11", "Test22", "Test33", "Test44"],
+    ])
+
+    const [answers] = useState([
+        "beeshk",
+        "test"
+    ])
+    
+    return (
+    <div className="flex flex-col items-center justify-center">
+        {questionText.map((text, index) => (
+            <Question questionText={text} choices={choices[index]} answer={answers[index]}/>
+        ))} 
     </div>
-)
+    )
+}
 
 export default QuizActivity;
