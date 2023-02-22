d7c0776026d4933503e41de7e22c5c7a2f854a6e
mslew <mlew3223@gmail.com>
2023-02-22 13:27:26 -0600

    selected is working

--- src/components/quiz/QuestionChoice.jsx
+++ src/components/quiz/QuestionChoice.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
-const QuestionChoice = ({choiceText}) => (
-    <button className="p-2 mb-3 ml-2 mr-2 text-gray-300 bg-gray-800 rounded-md shadow-md hover:bg-gray-600">{choiceText}</button>
-)
+function QuestionChoice (props){
+    const [isSelected, setSelected] = useState(false)
+    return(
+        <button className={`p-2 mb-3 ml-2 mr-2 text-gray-300 bg-gray-800 rounded-md shadow-md hover:bg-gray-600 ${isSelected ? "bg-gray-600" : "bg-gray-800"}`}
+        onClick={() => setSelected(!isSelected)}>{props.choiceText}</button>
+    )
+}
 
-export default QuestionChoice
+export default QuestionChoice;
