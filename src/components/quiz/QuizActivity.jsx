8907f5bd1362e5ac7b2f70ff951d14b5bc7a7b7b
mslew <mlew3223@gmail.com>
2023-02-22 23:16:35 -0600

    added header

--- src/components/quiz/QuizActivity.jsx
+++ src/components/quiz/QuizActivity.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import Question from "./Question";
 function QuizActivity({}){
+    const [category] = useState("TestCategory")
     const [questionText] = useState([
         "Test",
         "Test22"
@@ -19,6 +20,7 @@ function QuizActivity({}){
     
     return (
     <div className="flex flex-col items-center justify-center">
+        <h1 className="p-10 mb-8 text-4xl text-gray-300 bg-gray-900 rounded-lg shadow-lg">Welcome to the {category} Quiz</h1>
         {questionText.map((text, index) => (
             <Question questionText={text} choices={choices[index]} answer={answers[index]}/>
         ))} 
