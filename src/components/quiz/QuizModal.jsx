134b55e4c651e98bcfe68aa88adc93bd6d96743a
mslew <mlew3223@gmail.com>
2023-02-22 23:53:57 -0600

    fill color

--- src/components/quiz/QuizModal.jsx
+++ src/components/quiz/QuizModal.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import SquareX from "../icons/SquareX";
 
 const QuizModal = ({}) => (
+    <>
     <div className="grid grid-cols-1 grid-rows-3 absolute inset-x-0 z-10">
-        <div>Test <SquareX className={"h-10 w-10"}/></div>
+        <div>Test</div>
         <div>Test</div>
         <div>Test</div>
     </div>
+    <SquareX className={"h-10 w-10 fill-gray-300"}/>
+    </>
 )
 
 export default QuizModal
