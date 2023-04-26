8d038c208a1634ef42a74c8d685d275ce587fdf6
mslew <mlew3223@gmail.com>
2023-04-25 22:49:59 -0500

    comments in main

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+/**
+ * This is the main component.
+ * This hosts our main app component. 
+ * This mounts itself to the index.html root div
+ */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
