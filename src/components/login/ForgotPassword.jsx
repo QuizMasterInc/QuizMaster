be0f1dec3a84908883e560aa38a13a00ac599a97
Tyler Zenisek <tzenisekj@gmail.com>
2023-10-03 13:02:30 -0500

    cleaned up margins on login pages

--- src/components/login/ForgotPassword.jsx
+++ src/components/login/ForgotPassword.jsx
@@ -75,7 +75,7 @@ export default function ForgotPassword() {
                 <button
                   type="submit"
                   disabled={loading}
-                  className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  className="group mb-10 relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                   Reset Password
                 </button>
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -100,7 +100,7 @@ export default function Login() {
                     type="email"
                     autoComplete="email"
                     required
-                    className=" mt-3 relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    className=" mt-3 relative block w-full appearance-none rounded border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                     placeholder="Email"
                     ref={emailRef}
                   />
