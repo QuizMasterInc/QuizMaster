ff49534b82123589ed7c929cbf48d5794de55a1a
JoseMDO <joseamontesdeocamo@lewisu.edu>
2023-10-13 16:36:19 -0500

    added basic UI for custom quiz

--- src/components/customquiz/CustomQuiz.jsx
+++ src/components/customquiz/CustomQuiz.jsx
@@ -10,10 +10,14 @@ export default function CustomQuiz () {
    * state variables
    */
   const [error, setError] = useState('')
-  const {currentUser, logout, isGoogleAuth} = useAuth()
+  const {currentUser, logout, isGoogleAuth, getUserID} = useAuth()
   const [loading, setLoading] = useState(false)
   const {quizCategories, icons} = useCategory()
   const [results, setResults] = useState([])
+  const [finishedMakingQuiz, setFinishedMakingQuiz] = useState(false)
+  const [currentQuestion, setCurrentQuestion] = useState(["", "", "", "", "", ""]);
+  const [quizData, setQuizData] = useState([])
+  const [quizName, setQuizName] = useState("")
 
   /**
    * Logout function
@@ -36,34 +40,160 @@ export default function CustomQuiz () {
         <Navigate to="/signin" />
       )
     }
-}
+  }
 
+
+  // this creates the quiz object which we can use to send all the required data to the database
+  const createQuizObject = () => {
+    const createdDate = new Date().toLocaleDateString(undefined, { year: 'numeric', month: 'long', day: 'numeric' });
+    const userId = currentUser.uid
+    const questionCount = quizData.length
+    const quizObject = {
+        creator: userId,
+        createdDate: createdDate,
+        questionCount: questionCount,
+        quizName: quizName,
+        quizData: quizData,
+    }
+    //resets quiz questiOns to start a new quiz 
+    setQuizData([])
+    // WHEN A USER CLICKS FINISH QUIZ THIS LOGS THE OBJECT TO ENSURE IT IS CORRECT. USE THIS DATA ON DATABASE
+    console.log(quizObject);
+    return quizObject;
+  }
+
+  
+
+  //This function updates the information for each individual question
+  const handleQuestionChange = (e, index) => {
+    setCurrentQuestion((prevCurrentQuestion) => {
+      const updatedQuestion = [...prevCurrentQuestion];
+      updatedQuestion[index] = e.target.value;
+      return updatedQuestion;
+    })
+  };
+
+  //this function adds the question to the quizData array after user finished making the question 
+  const addCurrentQuestion = () => {
+    setQuizData((prevQuizData) => 
+    [
+      ...prevQuizData,
+      currentQuestion
+    ]);
+    setCurrentQuestion(["", "", "", "", "", ""]);
+  };
+
+
+  const handleQuizNameChange = (e) => {
+    setQuizName((prevQuizName) => {
+      let newName = prevQuizName;
+      newName = e.target.value;
+      return newName;
+    });
+  }
+
+  //Used this to make sure the proper quiz was added to the quizDataArray after pressing add question
+  useEffect(() => {
+    console.log(quizData); // This will show the updated state of quizData
+  }, [quizData]);
+  
 
     return (
     <>
-    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-            <h1 className="text-2xl font-bold text-gray-300">
-                Create a Custom Quiz!
-            </h1>
+      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-lg space-y-8">
           <div className='flex flex-col items-center justify-center'>
-            <Link to={'/quizzes'}>
-                  <div className='flex relative items-center mb-4 p-4 pl-8 pr-8 space-y-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20'>
-                    Take a premade quiz!
-                  </div>
-            </Link>
-            {!isGoogleAuth && <Link to={'/updateprofile'}>
-                  <div className='flex relative items-center mb-4 p-4 pl-8 pr-8 space-y-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20'>
-                    Update Profile
-                  </div>
-                  </Link>}
-            <button
+            <h1 className="text-2xl font-bold text-gray-300 mb-10">
+              Create a Custom Quiz!
+            </h1>
+            <div className='w-full'>
+
+              <div name="quizName">
+                <h1 className='text-white text-2xl mb-10'>First Type Your Quiz Name</h1>
+                <input 
+                type="text"
+                placeholder='Quiz Name'
+                className='text-2xl mb-4 rounded-md w-full h-12'
+                id="quizName"
+                value={quizName}
+                onChange={(e) => handleQuizNameChange(e)}
+                />
+              </div>
+
+              <div name="question-form" >
+                <h1 className='text-white text-2xl mb-8'>Now start adding your questions!</h1>
+                <input
+                  id="question"
+                  type="text"
+                  placeholder="Enter your question"
+                  value={currentQuestion[0]}
+                  onChange={(e) => handleQuestionChange(e, 0)}
+                  className='text-2xl mb-4 rounded-md w-full h-12'
+                  required
+                />
+                {[0, 1, 2, 3].map((optionIndex) => (
+                <div key={optionIndex}>
+                  <input
+                    id= {optionIndex + 1}
+                    type="text"
+                    placeholder={`Option ${optionIndex + 1}`}
+                    value={currentQuestion[optionIndex + 1]}
+                    onChange={(e) => handleQuestionChange(e, optionIndex + 1)}
+                    className='text-2xl mb-4 rounded-md w-full h-10'
+                    required
+                  />
+                </div>
+                ))}
+                <input
+                  id="right-awnser"
+                  type="text"
+                  placeholder='Enter the correct Awnser'
+                  value={currentQuestion[5]}
+                  onChange={(e) => handleQuestionChange(e, 5)}
+                  className='text-2xl mb-4 rounded-md w-full h-10'
+                  required
+                />
+              </div>
+            </div>
+            <div className='grid grid-cols-2 gap-y-3 gap-x-3 items-center justify-center'>
+              <div className='flex relative items-center mt-10 p-4 pl-8 pr-8 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20'>
+                <button type='submit' onClick={addCurrentQuestion}>Add Question</button>
+              </div>
+
+              <div className='flex relative items-center mt-10 p-4 pl-8 pr-8 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20'>
+                <button onClick={createQuizObject}>Finish Quiz</button>
+              </div>
+              <Link to={'/quizzes'}>
+                <div className='flex relative items-center p-4 pl-8 pr-8 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20'>
+                  Take a premade quiz!
+                </div>
+              </Link>
+              {!isGoogleAuth && <Link to={'/updateprofile'}>
+                <div className='flex relative items-center p-4 pl-8 pr-8 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600'>
+                  Update Profile
+                </div>
+                </Link>}
+
+              <button
                 disabled={loading}
                 onClick={handleLogout}
-                className="flex relative items-center p-4 pl-8 pr-8 space-y-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 -md:ml-20"
-            >
-              Logout
-            </button>
+                className="flex relative items-center p-4 pl-8 pr-8 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600"
+              >
+                Logout
+              </button>
+            </div>
+            <div id="questionsList" className='mt-10'>
+              {quizData.map((quiz, index) => (
+                <ul key={index} className='text-xl m-10'>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Question {index + 1}: {quizData[index][0]}</li>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Option 1: {quizData[index][1]}</li>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Option 2: {quizData[index][2]}</li>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Option 3: {quizData[index][3]}</li>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Option 4: {quizData[index][4]}</li>
+                  <li className='text-white flex justify-start max-w-lg break-words'>Right Awnser: {quizData[index][5]}</li>
+                </ul>
+              ))}
+            </div>
           </div>
         </div>
     </div>
