cfb7b0f8ee7c0ba308fe6aeea292156af653c4f3
Tyler Zenisek <tzenisekj@gmail.com>
2023-11-04 15:29:32 -0500

    worked on quiz editing page

--- functions/src/index.js
+++ functions/src/index.js
@@ -203,40 +203,37 @@ exports.deletedUser = functions.auth.user().onDelete(user => {
 })
 
 exports.grabCustomQuiz = functions.https.onRequest(async (req, res) => {
-    cors(req,res, async () => {
-        const dataType = req.get('content-type')
-        if(dataType === 'application/json'){
-            const { uid } = JSON.parse(JSON.stringify(req.body))
+    cors(req, res, async () => {
+        const  uid  = req.query.quizid
 
-            if (!uid) {
-                return res.status(401).json({
-                    result: false,
-                    message: "No UID field."
-                })
-            }
+        if (!uid) {
+            return res.status(401).json({
+                result: false,
+                message: "No UID field."
+            })
+        }
 
-            try {
-                const quiz = await admin.firestore().collection('custom_quizzes').doc(uid).get()
-                if (!quiz.exists) {
-                    return res.json({
-                        result: false,
-                        message: "Invalid UID"
-                    })
-                }
-                else {
-                    return res.json({
-                        result: true,
-                        status: 200,
-                        message: "quiz found",
-                        data: quiz.data()
-                    })
-                }
-            } catch(error) {
+        try {
+            const quiz = await admin.firestore().collection('custom_quizzes').doc(uid).get()
+            if (!quiz.exists) {
                 return res.json({
                     result: false,
-                    message: error.message
+                    message: "Invalid UID"
                 })
             }
+            else {
+                return res.json({
+                    result: true,
+                    status: 200,
+                    message: "quiz found",
+                    data: quiz.data()
+                })
+            }
+        } catch(error) {
+            return res.json({
+                result: false,
+                message: error.message
+            })
         }
     }) 
 })
--- src/components/customquiz/EditCustomQuiz.jsx
+++ src/components/customquiz/EditCustomQuiz.jsx
@@ -1,20 +1,53 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
+const getQuiz = async (uid) => {
+      //http://127.0.0.1:6001/quizmaster-c66a2/us-central1/grabCustomQuiz
+      // https://us-central1-quizmaster-c66a2.cloudfunctions.net/grabCustomQuiz
+      return await fetch('https://us-central1-quizmaster-c66a2.cloudfunctions.net/grabCustomQuiz?quizid=' + uid)
+        .then((res) => res.json())
+        .catch((err) => {
+          console.log("Respone Error: ", err.message);
+        })
+}
+
 export default function EditCustomQuiz() {
-  const [quiz, setQuiz] = useState({})
+  const [quiz, updateQuiz] = useState(null)
 
   const { quizID } = useParams()  // retrieves quiz ID from URL params 
 
+  const postQuestions = () => {
+    try {
+      Object.keys(quiz.questions).map((key, index) => {
+        return (
+          <div key={index}>
+            <h2>{key}</h2>
+          </div>
+        )
+      })
+    } catch(err) {
+      return (
+        <></>
+      )
+    }
+  }
+
   useEffect(() => {
     // this runs on component load to call https get request for quiz object from quiz ID
-
+    
+    getQuiz(quizID)
+    .then(quizData => {
+      console.log("Data Recieved: ", quizData.data)
+      console.log("Questions", quizData.data.questions["Question 1"].correct_answer)
+      updateQuiz(quizData.data)
+    })
   }, [])
 
   return (
     <div>
-      <h1>New Quiz</h1>
-      <h1>ID: {quizID}</h1>
+      <h1>{quiz?.title || "Loading..."}</h1>
+      <h2>{quiz?.numQuestions || "Loading..."}</h2>
+      <h2>{quiz?.quizTaken && "Loading..."}</h2>
     </div>
   )
 }
