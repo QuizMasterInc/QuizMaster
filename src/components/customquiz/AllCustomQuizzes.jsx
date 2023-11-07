0a7bd28a93d96cce0e15cc75e06bfca86849614b
juliedosher <juliegdosher@lewisu.edu>
2023-11-06 20:52:47 -0600

    added page for custom quizzes to go

---
+++ src/components/customquiz/AllCustomQuizzes.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const AllCustomQuizzes = () => {
+  return (
+    <div >
+        <h1 className="text-2xl font-bold text-gray-300 -sm:text-lg">User-Made Quizzes</h1>
+        <div className="flex flex-wrap justify-center">
+          
+        </div>
+    </div>
+  )
+}
+
+
+export default AllCustomQuizzes
