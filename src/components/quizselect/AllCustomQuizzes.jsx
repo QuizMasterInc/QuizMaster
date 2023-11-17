167a9abeb604a26b0ce5580312edf36d12f7123a
juliedosher <juliegdosher@lewisu.edu>
2023-11-17 01:37:50 -0600

    custom quizzes now show up on take a quiz page

--- src/components/quizselect/AllCustomQuizzes.jsx
+++ src/components/quizselect/AllCustomQuizzes.jsx
@@ -1,14 +1,77 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Link } from 'react-router-dom'
+import CustomQuizSelectButton from "./CustomQuizSelectButton";
 
 
 const AllCustomQuizzes = () => {
+	let [loading, setLoading] = useState(true)
+  	let [quizzes, setQuizzes] = useState([
+		{
+			"numQuestions": 0,
+			"createdAt": "",
+			"creator": "",
+			"questions": {},
+			"quizTaken": 0,
+			"title": "Loading...",
+			"lastEdit": "",
+			"tags": []
+	}])
+	  
+
+	/**
+	 * This useEffect() is used to grab all custom quizzes
+	 * We are using a Firebase cloud function (grabAllCustomQuizzes)
+	 */
+    useEffect(() => {
+      async function fetchCustomQuizzes() {
+        try {
+          //production: https://us-central1-quizmaster-c66a2.cloudfunctions.net/grabAllCustomQuizzes
+          //testing: http://127.0.0.1:6001/quizmaster-c66a2/us-central1/grabAllCustomQuizzes
+          const response = await fetch('https://us-central1-quizmaster-c66a2.cloudfunctions.net/grabAllCustomQuizzes', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+          });
+    
+          if (response.ok) {
+            const json = await response.json();
+            const quizData = json.data
+            setQuizzes(quizData)
+			
+
+          } else {
+            // Handle the case when the response is not ok (e.g., error handling)
+            console.error('Fetch error:', response.statusText);
+          }
+
+        } catch (error) {
+          // Handle any fetch-related errors here
+          console.error('Fetch error:', error);
+		}
+		setLoading(false)
+      }
+    
+      fetchCustomQuizzes();
+    }, []);
+
+	if (loading == true) {
+		console.log("hasn't loaded yet")
+		console.log("Title: " + quizzes[0].title)
+	}
+
+    if (loading == false) {
+		console.log("loading done")
+		console.log("Title: " + quizzes[0])
+	}
+	
   return (
-    <div >
-        <h1 className="text-2xl font-bold text-gray-300 -sm:text-lg">User-Made Quizzes</h1>
-        <div className="flex flex-wrap justify-center">
-            
-        </div>
+    <div>
+      	<h1 className="text-2xl font-bold text-gray-300 -sm:text-lg">User-Made Quizzes</h1>
+		<div className="flex flex-wrap justify-center mt-14">
+			{quizzes.map(q => (<CustomQuizSelectButton title={q.title} numQuestions={q.numQuestions} />))} 
+	  	</div>
     </div>
   )
 }
--- src/components/quizselect/CustomQuizSelectButton.jsx
+++ src/components/quizselect/CustomQuizSelectButton.jsx
@@ -1,19 +1,15 @@
 /**
- * This component hosts a button to click for each quiz category
+ * This component hosts a button to click for each custom quizz
  */
 import { Link } from "react-router-dom";
 
 
-const CustomQuizSelectButton = ({index, title, numquestions, destination}) => (
-    <div key={index} className="w-1/2 p-4 text-center -sm:p-1">
-        <Link to={'/customquiz/:uid/' + destination} state={{title}}>
-
-            grabCustomQuiz/?quizid=
-            <div className="flex flex-col items-center p-4 space-y-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600">
-                <div className="-sm:text-sm">{title}</div>
-                <div>{numquestions}</div>
-            </div>
-        </Link>
+const CustomQuizSelectButton = ({title, numQuestions}) => (
+    <div className="w-1/4 p-4 text-center -sm:p-1">
+        <div className="flex flex-col items-center p-4 space-y-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600">
+            <div className="-sm:text-sm">{title}</div>
+            <div className="-sm:text-sm">Questions: {numQuestions}</div>
+        </div>
     </div>
 )
 
