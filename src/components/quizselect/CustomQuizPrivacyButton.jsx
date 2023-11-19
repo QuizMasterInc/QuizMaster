c62730e87b7ca427f9a88ef7409a165f2350ebea
juliedosher <juliegdosher@lewisu.edu>
2023-11-18 20:46:26 -0600

    fixed wording

--- src/components/quizselect/CustomQuizPrivacyButton.jsx
+++ src/components/quizselect/CustomQuizPrivacyButton.jsx
@@ -15,8 +15,8 @@ class PrivacyRadioButtons extends Component {
     return (
       <div onChange={this.onChangeValue} class="text-white">
         <input type="radio" value="All" name="privacy" /> All Quizzes
-        <input type="radio" value="Public" name="privacy" /> Public Quizzes
-        <input type="radio" value="Private" name="privacy" /> Private Quizzes
+        <input type="radio" value="Public" name="privacy" /> Public
+        <input type="radio" value="Private" name="privacy" /> Private
       </div>
     );
   }
