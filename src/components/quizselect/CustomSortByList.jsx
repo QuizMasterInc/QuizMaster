9c07d70c3c12954f8a8aef8866d202376044cdb5
juliedosher <juliegdosher@lewisu.edu>
2023-11-27 10:50:27 -0600

    added "sort by" list, no functionality yet

--- src/components/quizselect/CustomSortByList.jsx
+++ src/components/quizselect/CustomSortByList.jsx
@@ -1,15 +1,36 @@
-export default function CustomSortByList() {
-  return (
-    <label class="text-white mx-2">
-      Sort by
-      <select name="listSortMethod" defaultValue="newest" class="text-black mx-2">
-        <option value="newest">Newest</option>
-        <option value="oldest">Oldest</option>
-        <option value="alpha">Title A-{">"}Z</option>
-        <option value="alphaReverse">Title Z-{">"}A</option>
-        <option value="shortest">Shortest</option>
-        <option value="longest">Longest</option>
-      </select>
-    </label>
-  );
+import React, { Component } from "react";
+
+
+class SortByList extends Component {
+  	constructor() {
+		super();
+		sessionStorage.setItem('sortingQuery', "newest")
+		this.onChangeValue = this.onChangeValue.bind(this);
+	}
+
+  	onChangeValue(event) {
+		sessionStorage.setItem('sortingQuery', event.target.value)
+    	console.log("sorting by: ", sessionStorage.getItem('sortingQuery'))
+    	return event.target.value
+ 	}
+
+
+	render() {
+    	return (
+			<div onChange={this.onChangeValue}> 
+				<label className="text-white mx-2">Sort by:
+					<select name="listSortMethod" defaultValue="newest" className="text-black mx-2">
+						<option value="newest">Newest</option>
+						<option value="oldest">Oldest</option>
+						<option value="alpha">Title A-{">"}Z</option>
+						<option value="alphaReverse">Title Z-{">"}A</option>
+						<option value="shortest">Shortest</option>
+						<option value="longest">Longest</option>
+					</select>
+				</label>
+			</div>
+    	);
+  	}
 }
+
+export default SortByList;
