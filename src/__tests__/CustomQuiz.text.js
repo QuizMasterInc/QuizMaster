31265cfb065e1c40b14b90a83262c334975c1d35
Jahi Stewart <jahikstewart@lewisu.edu>
2023-12-11 13:18:39 -0600

    CustomQuiz unit test: page renders

--- src/__tests__/CustomQuiz.text.js
+++ src/__tests__/CustomQuiz.text.js
@@ -1,63 +1,32 @@
-import {CustomQuiz} from '.././components/customquiz/CustomQuiz'
-
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // for better matchers
+import { render, waitFor } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import CustomQuiz from '../components/customquiz/CustomQuiz';
+import { AuthProvider } from '../contexts/AuthContext';
+
+// Import the AuthContext module directly
+import * as AuthContext from '../contexts/AuthContext';
 
 
-// Mock the useAuth hook for testing purposes
-jest.mock('../path-to-use-auth', () => ({
-  useAuth: jest.fn(() => ({ currentUser: { uid: 'mockUserId' } })),
-}));
-
-// Mock the fetch function to avoid actual network requests
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    ok: true,
-    json: () => Promise.resolve({ data: [] }),
-  })
-);
-
-describe('CustomQuiz Component', () => {
-  beforeEach(() => {
-    // Clear any mocks and reset component state before each test
-    jest.clearAllMocks();
+// Test your component
+test('renders without crashing', async () => {
+  // Mock the useAuth method
+  jest.spyOn(AuthContext, 'useAuth').mockReturnValue({
+    currentUser: { uid: 'mockUserId' },
+    setCurrentUser: jest.fn(),
+    setLoading: jest.fn(),
   });
 
-  it('renders without crashing', () => {
-    render(<CustomQuiz />);
-    // You might add more specific assertions based on your component structure
-  });
+  // Render the component
+  render(
+    <MemoryRouter>
+      <AuthProvider>
+        <CustomQuiz />
+      </AuthProvider>
+    </MemoryRouter>
+  );
 
-  it('fetches user quizzes on mount', async () => {
-    render(<CustomQuiz />);
-    // Wait for the component to fetch data
-    await waitFor(() => expect(global.fetch).toHaveBeenCalled());
-    // You might assert on the rendered content or the state after fetching
-  });
+  // No need for waitFor or additional expectations in this simple test
 
-  it('handles quiz name input correctly', () => {
-    const { getByLabelText, getByText } = render(<CustomQuiz />);
-    
-    // Simulate user input
-    fireEvent.change(getByLabelText(/quiz name/i), { target: { value: 'Test Quiz' } });
-
-    // You might assert on the state or the DOM to verify the behavior
-    expect(getByText(/test quiz/i)).toBeInTheDocument();
-  });
-
-  it('handles private quiz checkbox correctly', () => {
-    const { getByLabelText, getByText } = render(<CustomQuiz />);
-    
-    // Simulate user clicking on the checkbox
-    fireEvent.click(getByLabelText(/private quiz/i));
-
-    // You might assert on the state or the DOM to verify the behavior
-    expect(getByText(/private quiz/i)).toBeInTheDocument();
-  });
-
-  // Add more tests for other parts of your component's functionality
-
-  // For example, you can test the createQuizObject function, verifyQuizNameInput, etc.
-
+  // Your testing logic here if needed
 });
