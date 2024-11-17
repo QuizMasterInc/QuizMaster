6dfeb719ad84025460d48f622839ba5f753e3ed8
Demetrius-Price02 <maskedprofessor64@gmail.com>
2024-11-17 16:39:34 -0600

    Fixed welcome message and icons for quiz types

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -72,7 +72,7 @@ function Home() {
             <h1 className="pl-4 mt-10 text-3xl font-bold text-gray-300">
               You are now able to do everything Quiz Master has to offer. You can take 
               quizzes made by our developers or quizzes made from other users. You can
-              make your own quizzes to study for test or just test your knowledge. 
+              also make your own quizzes to study for tests or just test your knowledge. 
             </h1>
           </div>
         ) : (
--- src/components/typeofquiz/TypeOfQuiz.jsx
+++ src/components/typeofquiz/TypeOfQuiz.jsx
@@ -10,15 +10,15 @@ const TypeOfQuiz = () => {
         <div className="flex justify-center my-10 -sm:p-1">
             
             <Link to={'/quizzes'} >
-                <div className="p-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600">
-                    <QuizMasterIcon className={"w-10 h-10 fill-gray-300  -sm:w-8 -sm:h-8"} />
-                    <div className="-sm:text-sm">QuizMaster Quizzes</div>
+                <div className="p-4 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 flex flex-col items-center">
+                    <QuizMasterIcon className={"w-10 h-10 fill-gray-300  -sm:w-8 -sm:h-8 flex flex-col items-center"} />
+                    <div className="-sm:text-sm m-2">QuizMaster Quizzes</div>
                 </div>
             </Link>
             <Link to={'/allcustomquizzes'} >
-                <div className="p-4 mx-10 -sm:mx-2 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600">
+                <div className="p-4 mx-10 -sm:mx-2 text-gray-300 bg-gray-800 rounded-lg shadow-lg hover:shadow-xl hover:bg-gray-600 flex flex-col items-center">
                     <UserIcon className={"w-10 h-10 fill-gray-300  -sm:w-8 -sm:h-8"} />
-                    <div className="-sm:text-sm">User-Made Quizzes</div>
+                    <div className="-sm:text-sm m-2">User-Made Quizzes</div>
                 </div>
             </Link>
         </div>
