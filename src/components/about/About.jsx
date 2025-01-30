2a436e54c5631ff0241c6975e314624053c46fb2
nuviah8 <nuviahernandez@lewisu.edu>
2025-01-30 14:56:03 -0600

    Updated Information Page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,29 +8,30 @@ export default function About() {
     // Container div with styling classes
     <div className=" text-white py-24 sm:py-32">
       {/* Container for content */}
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-8xl px-6 lg:px-8">
         {/* Container for text content */}
         <div className="mx-auto max-w-2xl sm:text-center">
           {/* Heading */}
-          <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+          <h2 className="font-bold tracking-tight text-white sm:text-6xl mb-10">
             About Us
           </h2>
           {/* Description */}
-          <p className="mt-6 text-lg leading-8 text-white">
+          <p className="mt-5 text-2xl leading-8 text-white">
             Welcome to QuizMaster, your platform for managing, taking, 
             and creating quizzes!
           </p>
         </div>
         {/* Container for features */}
-        <div className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+        <div className="mx-auto mt-10 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
           {/* Left side: Features */}
           <div className="p-8 sm:p-10 lg:flex-auto">
             {/* Feature: QuizMaster */}
-            <h3 className="text-2xl font-bold tracking-tight text-white">
+            <h3 className="text-2xl font-bold tracking-tight text-white leading-10 text-center">
               QuizMaster {" "}
             </h3>
+            <div className="h-px flex-auto bg-gray-100"></div>  {/* Divider */}
             {/* Description of QuizMaster */}
-            <p className="mt-6 text-base leading-7 text-white">
+            <p className="mt-6 text-lg leading-7 text-white">
               Quiz Master is a website that has been built to allow users to take quizzes and test their knowledge before taking an exam.
               Why would someone be interested in trying out Quiz Master you might ask? 
               The answer is pretty straightforward from the name of the website, "Quiz Master".
@@ -39,21 +40,21 @@ export default function About() {
               Try our quizzes and put that brain into gear.
             </p>
             {/* Separator */}
-            <div className="mt-10 flex gap-x-4">
-              <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
+            <div className="mt-10 flex-none gap-x-4">
+              <h4 className="flex-auto text-2xl font-bold leading-10 text-white text-center">
                 What’s included
               </h4>
-              <div className="h-px flex-auto bg-gray-100"></div>
+              <div className="h-px flex-auto bg-gray-100"></div>  {/* Divider */}
             </div>
             {/* List of features */}
             <ul
               role="list"
-              className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-white sm:grid-cols-2 sm:gap-6"
+              className="mt-8 grid grid-cols-1 gap-4 text-base leading-6 text-white sm:grid-cols-2 sm:gap-6"
             >
               {/* Feature: Randomization */}
-              <li className="flex gap-x-3">
+              <li className="flex gap-x-3 text-left">
                 <svg
-                  className="h-6 w-5 flex-none text-indigo-600"
+                  className="h-6 w-5 flex-none text-white text-bold text-md"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -67,9 +68,9 @@ export default function About() {
                 Randomization of multiple choice questions
               </li>
               {/* Feature: Time Limit */}
-              <li className="flex gap-x-3">
+              <li className="flex gap-x-3 text-left">
                 <svg
-                  className="h-6 w-5 flex-none text-indigo-600"
+                  className="h-6 w-5 flex-none text-white text-bold text-md"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -83,9 +84,9 @@ export default function About() {
                 Set a time limit{" "}
               </li>
               {/* Feature: Immediate Results */}
-              <li className="flex gap-x-3">
+              <li className="flex gap-x-3 text-left">
                 <svg
-                  className="h-6 w-5 flex-none text-indigo-600"
+                  className="h-6 w-5 flex-none text-white text-bold text-md"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -99,9 +100,9 @@ export default function About() {
                 Immediate Results
               </li>
               {/* Feature: Option to hide correct answers */}
-              <li className="flex gap-x-3">
+              <li className="flex gap-x-3 text-left">
                 <svg
-                  className="h-6 w-5 flex-none text-indigo-600"
+                  className="h-6 w-5 flex-none text-white text-bold text-md"
                   viewBox="0 0 20 20"
                   fill="currentColor"
                   aria-hidden="true"
@@ -117,29 +118,34 @@ export default function About() {
             </ul>
           </div>
           {/* Right side: Team */}
-          <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
+          <div className="p-2 lg:mt-2 lg:w-full lg:max-w-lg lg:flex-shrink-0 lg:mr-2 lg:mb-2">
             {/* Container for team information */}
-            <div className="rounded-2xl bg-gray-400 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center lg:py-16">
+            <div className="rounded-2xl bg-gray-900 text-center lg:justify-center lg:py-10 lg:h-full lg:w-sm text-white">
               {/* Container for team details */}
-              <div className="mx-auto max-w-xs px-8">
+              <div className="mx-auto max-w-sm px-8">
                 {/* Heading */}
-                <p className="text-base font-semibold text-black">Our Team </p>
+                <h5 className="text-2xl font-bold text-white mb-1 mt-0"> 
+                  Our Team 
+                </h5>
+                <div className="h-px flex-auto bg-gray-100 "></div>  {/* Divider */}
                 {/* Description of team */}
-                <p className="mt-6 text-black  flex items-baseline justify-center gap-x-2">
+                <p className="mt-10 text-white flex items-baseline justify-center gap-x-2 text-xl">
                   Our talented team leverages React.js, Firebase hosting,
                   Tailwind CSS styling, and Golang for robust backend support.
                   We use Firestore to ensure seamless storage and updating of
                   quizzes and results.
                 </p>
+                <p className="mt-10 text-white flex items-baseline justify-center gap-x-2 text-xl mb-10">
+                Login or create an account to take advantage of all we have to offer!
+                </p>
+                <div className="h-px flex-auto bg-gray-100 "></div>  {/* Divider */}
                 {/* Call-to-action button */}
                 <a
                   href="/register"
-                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="mt-10 block w-sm rounded-md bg-slate-700 px-3 py-2 text-center text-lg font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get Started
+                  Get Started!
                 </a>
-                {/* Footer */}
-                <p className="mt-6 text-xs leading-5 text-white">QuizMaster</p>
               </div>
             </div>
           </div>
