f5188cdbe376105ba5a5036cc2af1d01008d8077
Demetrius-Price02 <maskedprofessor64@gmail.com>
2025-02-16 21:02:40 -0600

    idk

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
             Unlock the world of quizzes with QuizMaster, your ultimate
             destination for interactive learning.
           </p>
-          {!isAuthenticated && (
+          {!isAuthenticated && ( //Screen that shows when not logged in
             <div className="mt-6 flex justify-center space-x-4">
               <a
                 href="/register"
