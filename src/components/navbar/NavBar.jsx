d9078f9198f59de2b9d847250fd8cefb56d95b43
Samdwag <samialzoubi@lewisu.edu>
2025-03-08 17:13:29 -0600

    Fixed Navbar issue

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -76,17 +76,21 @@ export default function NavBar() {
         className="fixed top-16 left-4 z-50 p-2 bg-gray-800 text-white rounded-full shadow-lg hover:scale-110 transition-transform duration-300"
         onClick={toggleNav}
       >
-        {isNavOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+        {isNavOpen ? <FaTimes size={24} /> : <FaBars size={32} />}
       </button>
 
       {isNavOpen && (
         <div
           ref={navRef}
-          className="fixed w-24 h-[75vh] bg-gray-900 rounded-md shadow-lg left-2 top-32 space-y-10 overflow-y-auto no-scrollbar"
+          className="fixed w-24 h-[75vh] bg-gray-900 rounded-md shadow-lg hover:scale-100 left-2 top-32 space-y-10 overflow-y-auto no-scrollbar"
         >
-          <div className="flex flex-col items-center p-2 mt-4">
-            <FaBars size={32} className="text-white" />
-          </div>
+        <div
+          className="group flex flex-col items-center hover:scale-125 p-2 mt-4"
+          onClick={toggleNav}
+        >
+          <FaBars size={32} className="text-white group-hover:hidden" />
+          <FaTimes size={30} className="text-white hidden group-hover:block" />
+        </div>
           {currentUser && currentUser.role === "developer" && (
             <div className="hover:scale-125 duration-300">
               <NavLink
