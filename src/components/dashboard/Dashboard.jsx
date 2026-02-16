16bf9e48910c7fe1ddc3995c9fe0ceb441d328a3
Joseph Chavez <chavezjoseph696@gmail.com>
2026-02-15 18:05:01 -0600

    Fulfulling user story #326, As a user, I don't want to see Quizmaster made quizzes on the home page, so my experience is more personalized.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,12 @@
 /**
  * This is the dashboard parent component
  * this will only get mounted if the user is logged in
+ * commented imports are associated with the quizmaster quizzes section
  */
 import { useAuth } from '../../contexts/AuthContext';
-import { CategoryStatsCard } from './CategoryStatsCard';
+// import { CategoryStatsCard } from './CategoryStatsCard';
 import { Link, useLocation } from 'react-router-dom';
-import { QUIZ_CATEGORIES, CATEGORY_ICONS } from '../../constants/quizConstants.jsx';
+// import { QUIZ_CATEGORIES, CATEGORY_ICONS } from '../../constants/quizConstants.jsx';
 import RecentActivity from '../home/RecentActivity';
 
 export default function Dashboard() {
@@ -25,14 +26,20 @@ export default function Dashboard() {
         </section>
         <RecentActivity limit={6} />
 
-        <section className="dashboard-section">
+      {/*
+        comment below takes quizmaster quizzes off of the dashboard
+        not fully removed yet so don't delete until talked about more
+        -Joseph :)
+      */}
+
+        {/* <section className="dashboard-section">
           <h2 className="text-3xl font-bold text-center text-gradient-primary mb-10">Your Quiz Scores</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
             {QUIZ_CATEGORIES.map((category, index) => (
               <CategoryStatsCard key={index} category={category} icon={CATEGORY_ICONS[index]} />
             ))}
           </div>
-        </section>
+        </section> */}
 
         <section className="flex flex-col sm:flex-row justify-center gap-6 pt-10">
           <Link to="/typeofquiz" state={{ from: location.pathname }} className="inline-block px-8 py-3 bg-[var(--primary-400)] rounded-lg font-medium transition-all duration-200 shadow-lg hover:shadow-xl transform hover:scale-105 border-2 border-accent">
