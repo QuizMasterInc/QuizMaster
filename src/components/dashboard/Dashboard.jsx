2d8bb2406db02c617ed4ca6d54a101f8081485e6
Joseph Chavez <chavezjoseph696@gmail.com>
2026-03-28 21:45:48 -0500

    cleaning up the Dahsboard.jsx page of uncessary comments

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,11 +1,8 @@
 /**
  * This is the dashboard parent component
  * this will only get mounted if the user is logged in
- * commented imports are associated with the quizmaster quizzes section
  */
 import { useAuth } from '../../contexts/AuthContext';
-// import { CategoryStatsCard } from './CategoryStatsCard';
-// import { QUIZ_CATEGORIES, CATEGORY_ICONS } from '../../constants/quizConstants.jsx';
 import RecentActivity from '../home/RecentActivity';
 
 export default function Dashboard() {
@@ -23,21 +20,6 @@ export default function Dashboard() {
           </p>
         </section>
         <RecentActivity limit={6} />
-
-      {/*
-        comment below takes quizmaster quizzes off of the dashboard
-        not fully removed yet so don't delete until talked about more
-        -Joseph :)
-      */}
-
-        {/* <section className="dashboard-section">
-          <h2 className="text-3xl font-bold text-center text-gradient-primary mb-10">Your Quiz Scores</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-            {QUIZ_CATEGORIES.map((category, index) => (
-              <CategoryStatsCard key={index} category={category} icon={CATEGORY_ICONS[index]} />
-            ))}
-          </div>
-        </section> */}
       </div>
     </div>
   );
